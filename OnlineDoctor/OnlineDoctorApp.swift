71a591fe702867ded0af2b2766c68b9b76e3a6a8
chr1s78 <ayahirano2014@126.com>
2021-07-19 01:36:58 +0800

    Initial Commit

---
+++ OnlineDoctor/OnlineDoctorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineDoctorApp.swift
+//  OnlineDoctor
+//
+//  Created by 吕博 on 2021/7/19.
+//
+
+import SwiftUI
+
+@main
+struct OnlineDoctorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
